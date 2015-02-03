9dbb3f1b72c545a60ecf4ef30469795ee2616e5f
Levin Alexander <mail@levinalex.net>
2015-02-03 16:17:43 +0100

    initial import

---
+++ jj.go
@@ -0,0 +1,115 @@
+package jj
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Value struct {
+	data interface{}
+}
+
+type Type int
+
+const (
+	Null   = iota
+	Number = iota
+	String = iota
+	Object = iota
+	List   = iota
+)
+
+func getValueAtPath(o interface{}, keys ...interface{}) (interface{}, error) {
+	obj := o
+
+	for _, key := range keys {
+
+		switch key := key.(type) {
+		case string:
+			h, ok := obj.(map[string]interface{})
+			if !ok {
+				return nil, fmt.Errorf("jsvalue: cannot treat %v as a map", obj)
+			}
+			obj = h[key]
+		case int:
+			lst, ok := obj.([]interface{})
+			if !ok {
+				return nil, fmt.Errorf("jsvalue: cannot treat %v as a list", obj)
+			}
+			obj = lst[key]
+		default:
+			panic(fmt.Sprintf("jsvalue: key %#v is neither int nor string", key))
+		}
+	}
+	return obj, nil
+}
+
+func (v *Value) At(keys ...interface{}) *Value {
+	val, _ := getValueAtPath(v.data, keys...)
+	if val == nil {
+		return nil
+	} else {
+		return &Value{val}
+	}
+}
+
+func (v *Value) Type() Type {
+	if v == nil {
+		return Null
+	}
+	switch v.data.(type) {
+	case string:
+		return String
+	case float64:
+		return Number
+	case map[string]interface{}:
+		return Object
+	case []interface{}:
+		return List
+	default:
+		panic("unknown type")
+
+	}
+}
+
+func (v *Value) IsObject() bool {
+	return v.Type() == Object
+}
+
+func (v *Value) IsNumber() bool {
+	return v.Type() == Number
+}
+
+func (v *Value) IsString() bool {
+	return v.Type() == String
+}
+
+func (v *Value) IsNull() bool {
+	return v.Type() == Null
+}
+
+func (v *Value) IsList() bool {
+	return v.Type() == List
+}
+
+func (v *Value) String() string {
+	d, ok := v.data.(string)
+	if ok {
+		return d
+	} else {
+		panic(v.data)
+	}
+}
+
+func (v *Value) Number() int64 {
+	d, ok := v.data.(float64)
+	if ok {
+		return int64(d)
+	} else {
+		panic(v.data)
+	}
+}
+
+func (v *Value) UnmarshalJSON(b []byte) error {
+	return json.Unmarshal(b, &v.data)
+}
---
+++ jj_test.go
@@ -0,0 +1,89 @@
+package jj
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDeserialize(t *testing.T) {
+	var data Value
+	var err error
+	var str string
+
+	str = `{ "foo": "bar", "baz": 3, "bar": { "sub": "val", "int": 4 }}`
+	err = json.Unmarshal([]byte(str), &data)
+	assert.Nil(t, err)
+
+	assert.Equal(t, "bar", data.At("foo").String())
+	assert.Equal(t, "val", data.At("bar", "sub").String())
+	assert.Equal(t, "val", data.At("bar").At("sub").String())
+
+	assert.Equal(t, 4, data.At("bar", "int").Number())
+
+	str = `{ "foo": [1,2,"bar",{ "baz": "fred" }]}`
+	err = json.Unmarshal([]byte(str), &data)
+	assert.Nil(t, err)
+
+	assert.Equal(t, "bar", data.At("foo", 2).String())
+	assert.Equal(t, "fred", data.At("foo", 3, "baz").String())
+	assert.Equal(t, 1, data.At("foo", 0).Number())
+	assert.Equal(t, 2, data.At("foo", 1).Number())
+}
+
+func TestTypes(t *testing.T) {
+	var err error
+	var data *Value
+
+	err = json.Unmarshal([]byte(`{}`), &data)
+	assert.Nil(t, err)
+
+	assert.False(t, data.IsNumber())
+	assert.True(t, data.IsObject())
+	assert.False(t, data.IsString())
+	assert.False(t, data.IsNull())
+	assert.False(t, data.IsList())
+
+	err = json.Unmarshal([]byte(`12`), &data)
+	assert.Nil(t, err)
+
+	assert.True(t, data.IsNumber())
+	assert.False(t, data.IsObject())
+	assert.False(t, data.IsString())
+	assert.False(t, data.IsNull())
+	assert.False(t, data.IsList())
+
+	err = json.Unmarshal([]byte(`"foo"`), &data)
+	assert.Nil(t, err)
+
+	assert.False(t, data.IsNumber())
+	assert.False(t, data.IsObject())
+	assert.True(t, data.IsString())
+	assert.False(t, data.IsNull())
+	assert.False(t, data.IsList())
+
+	err = json.Unmarshal([]byte(`null`), &data)
+	assert.Nil(t, err)
+
+	assert.False(t, data.IsNumber())
+	assert.False(t, data.IsObject())
+	assert.False(t, data.IsString())
+	assert.True(t, data.IsNull())
+	assert.False(t, data.IsList())
+
+	err = json.Unmarshal([]byte(`[1,2,3]`), &data)
+	assert.Nil(t, err)
+
+	assert.False(t, data.IsNumber())
+	assert.False(t, data.IsObject())
+	assert.False(t, data.IsString())
+	assert.False(t, data.IsNull())
+	assert.True(t, data.IsList())
+
+	var dataNil *Value
+	assert.False(t, dataNil.IsNumber())
+	assert.False(t, dataNil.IsObject())
+	assert.False(t, dataNil.IsString())
+	assert.True(t, dataNil.IsNull())
+	assert.False(t, dataNil.IsList())
+}
